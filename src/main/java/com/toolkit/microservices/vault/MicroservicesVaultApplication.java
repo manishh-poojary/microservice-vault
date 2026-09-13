0826a6c8fe87b54032a85395528f2c5b05ad37ca
Manish Poojary <manishh.poojary02@gmail.com>
2026-09-13 12:10:00 +0530

    first commit

---
+++ src/main/java/com/toolkit/microservices/vault/MicroservicesVaultApplication.java
@@ -0,0 +1,13 @@
+package com.toolkit.microservices.vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toolkit/microservices/vault/MicroservicesVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolkit.microservices.vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
